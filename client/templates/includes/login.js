22f9d0c27a9833a6b15b0db64b12863a9ae55244
Flavio Uemura <flaviou@gmail.com>
2015-12-27 19:28:48 -0500

    Enhanced User maintenance security

--- client/templates/includes/login.js
+++ client/templates/includes/login.js
@@ -1,5 +1,5 @@
 Template._loginButtonsLoggedInDropdown.events({
     'click #login-buttons-edit-profile': function(event) {
-        Router.go('profileEdit');
+        Router.go("/profileEdit");
     }
 });
