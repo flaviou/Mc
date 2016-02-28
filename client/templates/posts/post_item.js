80843ac08509ef7aa34f6d742fb1bbb43566b4d4
Flavio Uemura <flaviou@gmail.com>
2016-02-28 16:20:16 -0500

    login page

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -1,4 +1,5 @@
 Template.postItem.helpers({
+/*
   popup_msg: function () {
     if (Meteor.user()) {
       return "";
@@ -6,15 +7,21 @@ Template.postItem.helpers({
       return "Login para votar";
     }
   },
+*/
   post_lead: function() {
-    return jQuery(this.content).text().split(' ').slice(0,29).join(' ') + '...';
+    return this.content.replace(/<(?:.|\n)*?>/gm, '').replace(/&nbsp;/g,'').split(' ').slice(0,30).join(' ').concat('...');
+//    return jQuery(this.content).text().split(' ').slice(0,30).join(' ').concat('...');
   }
 });
 
 Template.postItem.events({
-	"click .js-upvote":function(event){
-		var post = Posts.findOne({id: this.id});
-		Meteor.call('upvotePost', post);
-		return false;// prevent the button from reloading the page
-	}
+  "click .js-upvote":function(event){
+    if (Meteor.user()) {
+      var post = Posts.findOne({id: this.id});
+      Meteor.call('upvotePost', post);
+      return false;// prevent the button from reloading the page
+    } else {
+      window.location.href ="./login";
+    }
+  }
 });
